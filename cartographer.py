3faf178b666c96de5576f329ab4f8a8f67ebfa0d
Lizband <trash.block2@gmail.com>
2019-02-03 16:29:07 -0500

    Add game window

--- cartographer.py
+++ cartographer.py
@@ -2,3 +2,11 @@ import pygame
 
 # Important and need to be first
 pygame.init()
+
+# Resolution of window
+display_width = 800
+display_height = 600
+
+# Create window
+game_display = pygame.display.set_mode((display_width, display_height))
+
