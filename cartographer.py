a6f1edff364c42b039c12a927672acc096077078
Lizband <trash.block2@gmail.com>
2019-02-03 16:38:24 -0500

    Add game loop with ability to quit

--- cartographer.py
+++ cartographer.py
@@ -10,3 +10,12 @@ display_height = 600
 # Create window
 game_display = pygame.display.set_mode((display_width, display_height))
 
+
+# Game loop
+game_exit = False
+while not game_exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_exit = True
+
+pygame.quit()
